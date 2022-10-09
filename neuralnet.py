9aa14234f9692283f1c84d0255d017f27708d69c
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2022-10-09 22:13:36 +0300

    Fix

--- neuralnet.py
+++ neuralnet.py
@@ -44,21 +44,20 @@ def ESPCNx3():
 
 
 def ESPCNx4():
-    # X_in = Input(shape=(None, None, 1))
     X_in = Input(shape=(None, None, 3))
-    #X_interpolated = tf.keras.layers.UpSampling2D((4, 4), interpolation='bilinear')(X_in)
+
     X = Conv2D(filters=64, kernel_size=5, 
                kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
                padding='same', activation='tanh')(X_in)
     X = Conv2D(filters=32, kernel_size=3, 
                kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
                padding='same', activation='tanh')(X)
-    # X = Conv2D(filters=16, kernel_size=3, 
+               
     X = Conv2D(filters=48, kernel_size=3, 
                kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
                padding='same')(X)
+
     X = Lambda(pixel_shuffle(scale=4))(X)
-    #X = Conv2D(filters=3, kernel_size=3, padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.001))(X)
-    #X = X + X_interpolated
+    
     X_out = tf.clip_by_value(X, 0.0, 1.0)
     return Model(X_in, X_out, name="ESPCNx4")
