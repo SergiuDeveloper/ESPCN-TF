538040a4b192774cecaf1a917bc538d5d7039566
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2022-10-10 20:14:11 +0300

    Fix

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from tensorflow.keras.losses import MSE
 from datetime import datetime, timedelta
 import numpy as np
 import argparse
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--scale',     type=int, default=4,  help='-')
@@ -39,6 +40,8 @@ def main():
     ls_data = sorted_list("dataset/test/data")
     ls_labels = sorted_list("dataset/test/labels")
 
+    patch_size = 17
+
     sum_mse = 0
     sum_psnr = 0
     sum_ssim = 0
@@ -57,6 +60,12 @@ def main():
         hr_image = norm01(hr_image)
 
         lr_image = tf.expand_dims(lr_image, axis=0)
+        
+        lr_starting_y = random.randint(0, lr_patch.shape[0] - patch_size)
+        lr_starting_x = random.randint(0, lr_patch.shape[1] - patch_size)
+
+        lr_patch = lr_image[lr_starting_y:lr_starting_y+patch_size, lr_starting_x:lr_starting_x+patch_size]
+        hr_patch = lr_image[lr_starting_y*scale:(lr_starting_y+patch_size)*scale, lr_starting_x*scale:(lr_starting_x+patch_size)*scale]
 
         timestamp_before = datetime.now()
         cpu_usage_before = None
@@ -72,7 +81,7 @@ def main():
         except:
             pass
 
-        sr_image = model.predict(lr_image)[0]
+        sr_patch = model.predict(lr_patch)[0]
 
         timestamp_after = datetime.now()
         try:
@@ -80,9 +89,9 @@ def main():
         except:
             pass
 
-        sum_mse += tf.reduce_mean(MSE(hr_image, sr_image).numpy())
-        sum_psnr += PSNR(hr_image, sr_image).numpy()
-        sum_ssim += SSIM(hr_image, sr_image).numpy()
+        sum_mse += tf.reduce_mean(MSE(hr_patch, sr_patch).numpy())
+        sum_psnr += PSNR(hr_patch, sr_patch).numpy()
+        sum_ssim += SSIM(hr_patch, sr_patch).numpy()
         sum_runtime += timestamp_after - timestamp_before
         if gpu_usage_before is not None and gpu_usage_after is not None:
             sum_gpu_usage += gpu_usage_after - gpu_usage_before
