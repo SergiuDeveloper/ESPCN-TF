2c31b21e59bceed1ac4108b43371279d83c764ab
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2022-10-18 02:15:41 +0300

    Fix

--- neuralnet.py
+++ neuralnet.py
@@ -54,7 +54,7 @@ def ESPCNx4():
                padding='same', activation='tanh')(X)
     X = Conv2D(filters=48, kernel_size=3, 
                kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
-               padding='same', activation='sigmoid')(X)
+               padding='same', activation='tanh')(X)
     X = Lambda(pixel_shuffle(scale=4))(X)
     
     X_out = tf.clip_by_value(X, 0.0, 1.0)
