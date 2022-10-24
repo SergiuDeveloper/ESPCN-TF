9ce254aa5bc87e8fe34f3626b8a5c9cc2a8d7113
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2022-10-24 12:27:21 +0300

    Fix

--- neuralnet.py
+++ neuralnet.py
@@ -46,13 +46,13 @@ def ESPCNx3():
 def ESPCNx4():
     X_in = Input(shape=(None, None, 3))
 
-    X = Conv2D(filters=256, kernel_size=5, 
-               kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
-               padding='same', activation='tanh')(X_in)
-    X = Conv2D(filters=128, kernel_size=5, 
+    X = Conv2D(filters=64, kernel_size=5, 
                kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
                padding='same', activation='tanh')(X)
-    X = Conv2D(filters=64, kernel_size=5, 
+    X = Conv2D(filters=32, kernel_size=3, 
+               kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
+               padding='same', activation='tanh')(X)
+    X = Conv2D(filters=16, kernel_size=3, 
                kernel_initializer=RandomNormal(mean=0, stddev=0.001), 
                padding='same', activation='tanh')(X)
     X = Conv2D(filters=32, kernel_size=3, 
